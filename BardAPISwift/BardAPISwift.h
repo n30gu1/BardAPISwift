fdd5a9ed1f79d52c83e593c467638e137d9e568f
n30gu1 <n@n30gu1.com>
2023-07-10 17:15:34 +0900

    Initial Commit

---
+++ BardAPISwift/BardAPISwift.h
@@ -0,0 +1,18 @@
+//
+//  BardAPISwift.h
+//  BardAPISwift
+//
+//  Created by Sung Park on 7/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BardAPISwift.
+FOUNDATION_EXPORT double BardAPISwiftVersionNumber;
+
+//! Project version string for BardAPISwift.
+FOUNDATION_EXPORT const unsigned char BardAPISwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BardAPISwift/PublicHeader.h>
+
+
